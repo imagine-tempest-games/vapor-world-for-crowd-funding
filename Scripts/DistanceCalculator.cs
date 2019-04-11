1b09a1664d16cf94d0282a19cc98738536f38811
skulllism <editer77@gmail.com>
2019-04-11 22:12:52 +0900

    Refine post-effects

---
+++ Scripts/DistanceCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using AdvancedUnityPlugin;
+
+public class DistanceCalculator : MonoBehaviour
+{
+    public GameObject target;
+    public float distance;
+
+    public FloatUnityEvent onLessThan;
+
+    public void Update()
+    {
+        if (!target)
+            return;
+
+        float result = Vector2.Distance(this.transform.position, target.transform.position);
+        if (Mathf.Abs(result) < Mathf.Abs(distance))
+            onLessThan.Invoke(result);
+    }
+}
