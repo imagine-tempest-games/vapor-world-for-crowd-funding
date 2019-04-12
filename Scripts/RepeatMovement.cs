7e40132fde97d9cbcf3cc6eca151ae74e762cdf7
최희렬 <choehuilyeol@choehuilyeol-ui-MacBook-Pro.local>
2019-04-12 21:19:19 +0900

    Created Movement tree for 'Scene#5'

---
+++ Scripts/RepeatMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 speed;
+
+    private Transform myTrans;
+    private Vector3 originPos;
+
+    private void Awake()
+    {
+        myTrans = GetComponent<Transform>();
+
+        originPos = myTrans.position;
+    }
+
+
+    private void Update()
+    {
+        myTrans.Translate(speed * Time.deltaTime);
+
+        float dis = Vector2.Distance(target.transform.position, myTrans.position);
+        if(Mathf.Abs(dis) <= 1.0f)
+        {
+            myTrans.position = originPos;
+        }
+    }
+}
