5cfedb210365b6fbfe76ee4a5ebfa78512ad1c55
skulllism <editer77@gmail.com>
2019-04-12 14:53:52 +0900

    fixed dust particle

---
+++ Scripts/ParticleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public Material material;
+    public new ParticleSystem particleSystem;
+
+    public float originAlpha;
+
+    public void Awake()
+    {
+        originAlpha = material.GetColor("_TintColor").a * 100.0f;
+    }
+
+    public void SetColorAlpha(float alpha)
+    {
+        float value = (originAlpha) - (alpha * 0.01f);
+        if (value > originAlpha)
+            value = originAlpha;
+
+        Color color = material.GetColor("_TintColor");
+
+        color.a = value;
+
+        material.SetColor("_TintColor", color);
+    }
+}
